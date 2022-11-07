4ff116c1fd0cb1137a47364baf94224d3821a124
Maciej <mszakacz@gmail.com>
2022-11-07 18:58:45 +0100

    feat(api_client): generate SpritesData.g

---
+++ packages/api_client/lib/src/models/sprites_data.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sprites_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SpritesData _$SpritesDataFromJson(Map<String, dynamic> json) => SpritesData(
+      backDefault: json['back_default'] as String?,
+      backFemale: json['back_female'] as String?,
+      backShiny: json['back_shiny'] as String?,
+      backShinyFemale: json['back_shiny_female'] as String?,
+      frontDefault: json['front_default'] as String?,
+      frontFemale: json['front_female'] as String?,
+      frontShiny: json['front_shiny'] as String?,
+      frontShinyFemale: json['front_shiny_female'] as String?,
+    );
