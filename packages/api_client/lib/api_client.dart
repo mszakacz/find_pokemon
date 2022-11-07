0557ce2721d9a89317e6c545ff4e38aa16e97c95
Maciej <mszakacz@gmail.com>
2022-11-07 18:53:45 +0100

    feat(api_client): create api_client package

---
+++ packages/api_client/lib/api_client.dart
@@ -0,0 +1,4 @@
+library api_client;
+
+export './src/models/models.dart';
+export 'src/api_client.dart';
---
+++ packages/api_client/test/src/api_client_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:api_client/api_client.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('ApiClient', () {
+    test('can be instantiated', () {
+      expect(ApiClient(), isNotNull);
+    });
+  });
+}
