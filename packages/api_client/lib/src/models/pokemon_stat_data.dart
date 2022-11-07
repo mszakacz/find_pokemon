08ac2f1ca3a685e1df3d0883a920ea6cbb813ea1
Maciej <mszakacz@gmail.com>
2022-11-07 18:55:24 +0100

    feat(api_client): create PokemonStatData model

---
+++ packages/api_client/lib/src/models/pokemon_stat_data.dart
@@ -0,0 +1,32 @@
+import 'package:api_client/api_client.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'pokemon_stat_data.g.dart';
+
+@JsonSerializable(createToJson: false)
+
+/// Pokemon Stat Data
+class PokemonStatData {
+  /// const constructor of the PokemonStatData model
+  const PokemonStatData({
+    required this.value,
+    required this.effort,
+    required this.stat,
+  });
+
+  /// it returns PokemonStatData from json
+  factory PokemonStatData.fromJson(Map<String, dynamic> json) =>
+      _$PokemonStatDataFromJson(json);
+
+  /// stat base value
+  @JsonKey(name: 'base_stat')
+  final int value;
+
+  /// stat effort
+  @JsonKey(name: 'effort')
+  final int effort;
+
+  /// stat
+  @JsonKey(name: 'stat')
+  final StatData stat;
+}
