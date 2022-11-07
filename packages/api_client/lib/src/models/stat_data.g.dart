41805ba840be5d95042501c763e1a2eea590cbdd
Maciej <mszakacz@gmail.com>
2022-11-07 18:59:08 +0100

    feat(api_client): generate StatData.g

---
+++ packages/api_client/lib/src/models/stat_data.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'stat_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+StatData _$StatDataFromJson(Map<String, dynamic> json) => StatData(
+      name: json['name'] as String,
+      url: json['url'] as String,
+    );
