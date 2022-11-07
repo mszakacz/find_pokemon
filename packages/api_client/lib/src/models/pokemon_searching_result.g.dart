fcd0ee0ad2290b75e69ac8b92ee7891834ca5595
Maciej <mszakacz@gmail.com>
2022-11-07 18:55:10 +0100

    feat(api_client): generate PokemonSearchingResult.g

---
+++ packages/api_client/lib/src/models/pokemon_searching_result.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pokemon_searching_result.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PokemonSearchingResult _$PokemonSearchingResultFromJson(
+  Map<String, dynamic> json,
+) =>
+    PokemonSearchingResult(
+      name: json['name'] as String,
+      url: json['url'] as String,
+    );
