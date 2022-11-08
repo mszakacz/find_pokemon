fdd7d4f1ceb9576857aaaceb0396b62697378680
Maciej <mszakacz@gmail.com>
2022-11-08 13:02:51 +0100

    test(main): call searchPokemons for development purposes

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,6 +5,9 @@ import 'package:pokemon/bootstrap.dart';
 Future<void> main() async {
   final apiClient = ApiClient();
 
+  final pokemonName = await apiClient.searchPokemons(phrase: 'ditto');
+  print('pokemon name: ${pokemonName}');
+
   await bootstrap(
     () => const App(),
   );
