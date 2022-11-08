2c109e4c2daf5f2f6b2177f1fb2985f4fb4ade10
Maciej <mszakacz@gmail.com>
2022-11-08 13:04:37 +0100

    refactor(api_client): use test library instead of flutter_test for testing

--- packages/api_client/test/src/models/sprites_data_test.dart
+++ packages/api_client/test/src/models/sprites_data_test.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: avoid_redundant_argument_values
 
 import 'package:api_client/api_client.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('SpritesData', () {
