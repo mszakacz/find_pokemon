de5b6f951cd4c464be85618dbdac8364c3c41ec5
Maciej <mszakacz@gmail.com>
2022-11-08 21:58:21 +0100

    refactor(pokemons_repository): rename pokemonStats method

--- packages/pokemons_repository/lib/src/service/pokemon_stats.dart
+++ packages/pokemons_repository/lib/src/service/pokemon_stats.dart
@@ -2,7 +2,7 @@ import 'package:pokemons_api/pokemons_api.dart';
 import 'package:pokemons_repository/pokemons_repository.dart';
 
 /// returns the list of pokemon stats [List]<[Stat]>
-List<Stat> pokemonsStats({
+List<Stat> pokemonStats({
   required List<PokemonStatData> stats,
 }) {
   final pokemonStats = <Stat>[];
