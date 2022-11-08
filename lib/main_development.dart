d972c6aa533c9875a7e9b9c02fb8438d1f379dd0
Maciej <mszakacz@gmail.com>
2022-11-08 14:38:42 +0100

    feat(main): initialize PokemonsRepository

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,13 +1,17 @@
 import 'package:pokemon/app/app.dart';
 import 'package:pokemon/bootstrap.dart';
-
 import 'package:pokemons_api/pokemons_api.dart';
+import 'package:pokemons_repository/pokemons_repository.dart';
 
 Future<void> main() async {
   final pokemonsApiClient = PokemonsApiClient();
 
-  final pokemonName = await pokemonsApiClient.searchPokemons(phrase: 'ditto');
-  print('pokemon name: ${pokemonName}');
+  final pokemonsRepository = PokemonsRepository(
+    pokemonsApiClient: pokemonsApiClient,
+  );
+
+  final pokemon = await pokemonsRepository.getPokemon(name: 'ditto');
+  print('pokemon name: ${pokemon.name}');
 
   await bootstrap(
     () => const App(),
