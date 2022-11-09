71fd539a487c4800357ad2142dc6237d261bf306
Maciej <mszakacz@gmail.com>
2022-11-09 16:12:00 +0100

    feat(main): initialize shearedPreferences

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,13 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:pokemon/app/app.dart';
 import 'package:pokemon/bootstrap.dart';
 import 'package:pokemons_api/pokemons_api.dart';
 import 'package:pokemons_repository/pokemons_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final pokemonsApiClient = PokemonsApiClient();
+
+  final sharedPreferences = await SharedPreferences.getInstance();
 
   final pokemonsRepository = PokemonsRepository(
     pokemonsApiClient: pokemonsApiClient,
+    sharedPreferences: sharedPreferences,
   );
 
   await bootstrap(
