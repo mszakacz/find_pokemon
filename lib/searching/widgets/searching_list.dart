9a0c01db8ad2eb68fd96fc19292613e5eb215bbc
Maciej <mszakacz@gmail.com>
2022-11-10 09:17:55 +0100

    refactor(searching): Searching List has _ListTile widget

--- lib/searching/widgets/searching_list.dart
+++ lib/searching/widgets/searching_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'package:pokemon/widgets/widgets.dart';
+import 'package:pokemon/pokemon_details/pokemon_details.dart';
+import 'package:pokemon/theme/theme.dart';
+import 'package:pokemon/utils/utils.dart';
 
 class SearchingList extends StatelessWidget {
   const SearchingList({
@@ -14,12 +15,40 @@ class SearchingList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.separated(
       itemBuilder: (BuildContext context, int index) {
-        return PokemonListTile(pokemonName: pokemons[index]);
+        return _ListTile(pokemonName: pokemons[index]);
       },
       itemCount: pokemons.length,
       separatorBuilder: (context, index) {
         return const Divider(
           height: 2,
+        );
+      },
+    );
+  }
+}
+
+class _ListTile extends StatelessWidget {
+  const _ListTile({
+    required this.pokemonName,
+  });
+
+  final String pokemonName;
+
+  @override
+  Widget build(BuildContext context) {
+    final displayName = pokemonName.asPokemonName();
+    return ListTile(
+      title: Text(
+        displayName,
+        style: Theme.of(context).textTheme.headline3,
+      ),
+      tileColor: AppColors.darkScaffold,
+      textColor: AppColors.white,
+      onTap: () {
+        Navigator.of(context).push(
+          PokemonDetailsPage.route(
+            pokemonName: pokemonName,
+          ),
         );
       },
     );
